6ec2f53eb81dcb0c889d906db018c5d690b86d48
AltairQ <maciejkamat@gmail.com>
2019-04-06 22:47:48 +0200

    Quick prototype

---
+++ Limbo/LimboExt.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+
+using KeePass.Forms;
+using KeePass.Plugins;
+using KeePass.Resources;
+using KeePass.UI;
+
+using KeePassLib;
+using KeePassLib.Security;
+using KeePassLib.Utility;
+
+using Newtonsoft.Json;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace Limbo
+{
+    public sealed class LimboExt : Plugin
+    {
+        private IPluginHost m_host = null;
+
+        public override bool Initialize(IPluginHost host)
+        {
+            if (host == null) return false;
+            m_host = host;
+            return true;
+        }
+
+        public override ToolStripMenuItem GetMenuItem(PluginMenuType t)
+        {
+            if (t != PluginMenuType.Main) return null;
+
+            ToolStripMenuItem tsmi = new ToolStripMenuItem("Limbo");
+
+            ToolStripMenuItem anal = new ToolStripMenuItem("Analyze");
+            anal.Click += OnAnalClick;
+
+            tsmi.DropDownItems.Add(anal);
+
+            return tsmi;
+
+        }
+
+        private void OnAnalClick(object sender, EventArgs e)
+        {
+            PwGroup grp = m_host.Database.RootGroup;
+            var entries = grp.GetEntries(true);
+
+            List<Dictionary<string, string>> lst = new List<Dictionary<string, string>>();
+
+
+            SHA1 sha = new SHA1CryptoServiceProvider();
+
+            foreach (var entry in entries)
+            {
+                Dictionary<string, string> entry_dict = new Dictionary<string, string>();
+                entry_dict.Add("username", entry.Strings.ReadSafe(PwDefs.UserNameField));
+
+                var passwordHash = string.Join("", sha.ComputeHash(entry.Strings.Get(PwDefs.PasswordField).ReadUtf8()).Select(x => x.ToString("x2"))).ToUpperInvariant();
+
+                entry_dict.Add("pass_sha1", passwordHash);
+                entry_dict.Add("url", entry.Strings.ReadSafe(PwDefs.UrlField));
+
+                lst.Add(entry_dict);
+            }
+            
+            var outs = JsonConvert.SerializeObject(lst);
+
+            MessageService.ShowInfo(outs);
+
+            SockSend(outs);
+
+        }
+
+        public static void SockSend(string out_msg)
+        {
+
+            // Connect to a remote device.  
+            try
+            {
+                // Establish the remote endpoint for the socket.  
+                // This example uses port 11000 on the local computer.  
+                IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 2137);
+
+                // Create a TCP/IP  socket.  
+                Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.  
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array.  
+                    byte[] msg = Encoding.ASCII.GetBytes(out_msg);
+
+                    // Send the data through the socket.  
+                    int bytesSent = sender.Send(msg);
+
+                   
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    MessageService.ShowFatal("ArgumentNullException : {0}" + ane);
+                }
+                catch (SocketException se)
+                {
+                    MessageService.ShowFatal("SocketException : {0}" + se);
+                }
+                catch (Exception e)
+                {
+                    MessageService.ShowFatal("Unexpected exception : {0}" + e);
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
